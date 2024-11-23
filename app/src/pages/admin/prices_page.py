99787e221eb4326b25143377b0e9cfe34987621e
Kazu0210 <clffrdkyl@gmail.com>
2024-11-23 22:19:27 +0800

    added widgets

--- app/src/pages/admin/prices_page.py
+++ app/src/pages/admin/prices_page.py
@@ -1,5 +1,9 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QTableWidgetItem
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QBrush, QColor
 from ui.NEW.prices_page import Ui_Form as Ui_price_page
+
+import json, pymongo, datetime, re
 
 class PricesPage(QWidget, Ui_price_page):
     def __init__(self, parent_window=None):
@@ -7,4 +11,94 @@ class PricesPage(QWidget, Ui_price_page):
         self.setupUi(self)
         self.parent_window = parent_window
 
-        # show all prices in this page
+        self.load_all()
+
+    def load_all(self):
+        """Load all the widgets that need to be updated once or multiple times"""
+        self.load_prices()
+
+    def load_prices(self):
+        """Load prices current price on the prices table"""
+        table = self.prices_tableWidget
+        vertical_header = table.verticalHeader()
+        vertical_header.hide()
+        table.setRowCount(0)  # Clear the table
+
+        # header json directory
+        header_dir = "app/resources/config/table/accounts_tableHeader.json"
+
+        # settings directory
+        settings_dir = "app/resources/config/settings.json"
+
+        with open(header_dir, 'r') as f:
+            header_labels = json.load(f)
+
+        table.setColumnCount(len(header_labels))
+        table.setHorizontalHeaderLabels(header_labels)
+
+        for column in range(table.columnCount()):
+            table.setColumnWidth(column, 200)
+
+        # Clean the header labels
+        self.header_labels = [self.clean_header(header) for header in header_labels]
+
+        # Filters
+        # filter_query = {}
+        # job_filter = self.job_filter.currentText()
+        # account_status_filter = self.account_status_filter.currentText()
+
+        if job_filter != "Show All":
+            filter_query['job'] = job_filter
+
+        if account_status_filter != "Show All":
+            filter_query['status'] = account_status_filter
+
+        # Get data from MongoDB
+        data = list(self.collection.find(filter_query).sort("_id", -1))
+        if not data:
+            return  # Exit if the collection is empty
+        
+        with open(settings_dir, 'r') as f:
+            settings = json.load(f)
+            self.current_time_format = settings['time_date'][0]['time_format']
+        
+        # Populate table with data
+        for row, item in enumerate(data):
+            table.setRowCount(row + 1)  # Add a new row for each item
+            for column, header in enumerate(self.header_labels):
+                original_keys = [k for k in item.keys() if self.clean_key(k) == header]
+                original_key = original_keys[0] if original_keys else None
+                value = item.get(original_key)
+                if value is not None:
+                    if header == 'lastlogin':
+                        try:
+                            if value:
+                                date_time = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+
+                                if self.current_time_format == "12hr":
+                                    value = date_time.strftime("%Y-%m-%d %I:%M:%S %p")
+                                else:
+                                    value = date_time.strftime("%Y-%m-%d %H:%M:%S")
+                        except Exception as e:
+                            pass
+                            # print(f"Error formatting date: {e}")
+                    table_item = QTableWidgetItem(str(value))
+                    table_item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)  # Center the text
+                    # check if row index is even
+                    if row % 2 == 0:
+                        table_item.setBackground(QBrush(QColor("#F6F6F6"))) # change item's background color to #F6F6F6 when row index is even
+                            
+                    table.setItem(row, column, table_item)
+
+    def connect_to_db(self, collection_name):
+        connection_string = "mongodb://localhost:27017/"
+        client = pymongo.MongoClient(connection_string)
+        db = "LPGTrading_DB"
+        return client[db][collection_name]
+    
+    def clean_key(self, key):
+        return re.sub(r'[^a-z0-9]', '', key.lower().replace(' ', '').replace('_', ''))
+
+    def clean_header(self, header):
+        return re.sub(r'[^a-z0-9]', '', header.lower().replace(' ', '').replace('_', ''))
+    
--- app/src/ui/NEW/prices_page.py
+++ app/src/ui/NEW/prices_page.py
@@ -13,9 +13,20 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(640, 480)
-        self.label = QtWidgets.QLabel(parent=Form)
-        self.label.setGeometry(QtCore.QRect(10, 10, 81, 16))
-        self.label.setObjectName("label")
+        self.prices_tableWidget = QtWidgets.QTableWidget(parent=Form)
+        self.prices_tableWidget.setGeometry(QtCore.QRect(10, 40, 621, 192))
+        self.prices_tableWidget.setObjectName("prices_tableWidget")
+        self.prices_tableWidget.setColumnCount(0)
+        self.prices_tableWidget.setRowCount(0)
+        self.searchBar_lineEdit = QtWidgets.QLineEdit(parent=Form)
+        self.searchBar_lineEdit.setGeometry(QtCore.QRect(10, 10, 271, 20))
+        self.searchBar_lineEdit.setObjectName("searchBar_lineEdit")
+        self.search_pushButton = QtWidgets.QPushButton(parent=Form)
+        self.search_pushButton.setGeometry(QtCore.QRect(290, 10, 75, 23))
+        self.search_pushButton.setObjectName("search_pushButton")
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(10, 240, 111, 23))
+        self.pushButton.setObjectName("pushButton")
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -23,7 +34,9 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.label.setText(_translate("Form", "Prices Page"))
+        self.searchBar_lineEdit.setPlaceholderText(_translate("Form", "Type Product ID or Product Name Here"))
+        self.search_pushButton.setText(_translate("Form", "Search"))
+        self.pushButton.setText(_translate("Form", "View Price History"))
 
 
 if __name__ == "__main__":
