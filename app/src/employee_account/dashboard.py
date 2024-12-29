7cd0cd8688bf408380322868f82017dd56f6a7f6
nycleee <ravelonicole02@gmail.com>
2024-12-29 23:20:17 +0800

    pending papo

--- app/src/employee_account/dashboard.py
+++ app/src/employee_account/dashboard.py
@@ -1,8 +1,8 @@
 from PyQt6.QtWidgets import QWidget
-from ui.employee.dashboard import Ui_Form as employee_dashboard_UI
+from ui.employee.dashboard_page import Ui_Form as dashboard_employee_page
 
 class employee_dashboard(QWidget):
     def __init__(self):
         super(employee_dashboard, self).__init__()
-        self.ui = employee_dashboard_UI()
+        self.ui = dashboard_employee_page()
         self.ui.setupUi(self)
