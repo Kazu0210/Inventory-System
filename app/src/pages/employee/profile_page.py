0c45485d6a05370d316cd81e4e627c1f8c3f10da
Kazu0210 <clffrdkyl@gmail.com>
2025-01-02 22:10:08 +0800

    `Updated ProfilePage class and related files with removed code`

--- app/src/pages/employee/profile_page.py
+++ app/src/pages/employee/profile_page.py
@@ -18,12 +18,12 @@ class ProfilePage(QWidget, profile_page):
         self.username = username  # Store the username for use in methods
 
         # button connections
-        self.updateProfile_pushButton.clicked.connect(lambda: self.show_update_profile_form()) # show update profile form
-        self.updatePassword_pushButton.clicked.connect(lambda: self.show_update_pass_form()) # show password form
-        self.cancel_update_pushButton.clicked.connect(lambda: self.hide_update_profile_form()) # cancel update profile info
-        self.cancel_password_pushButton.clicked.connect(lambda: self.cancel_password_clicked()) # cancel create password
-        self.updatePass_pushButton.clicked.connect(lambda: self.save_new_password()) # save new password
-        self.update_pushButton.clicked.connect(lambda: self.save_user_info()) # save new user info
+        # self.updateProfile_pushButton.clicked.connect(lambda: self.show_update_profile_form()) # show update profile form
+        self.password_btn.clicked.connect(lambda: self.show_update_pass_form()) # show password form
+        # self.cancel_update_pushButton.clicked.connect(lambda: self.hide_update_profile_form()) # cancel update profile info
+        # self.cancel_password_pushButton.clicked.connect(lambda: self.cancel_password_clicked()) # cancel create password
+        # self.updatePass_pushButton.clicked.connect(lambda: self.save_new_password()) # save new password
+        # self.update_pushButton.clicked.connect(lambda: self.save_user_info()) # save new user info
 
         self.display_user_info()
         self.hide_update_profile_form()
@@ -67,7 +67,7 @@ class ProfilePage(QWidget, profile_page):
                 if result.modified_count > 0:
                     QMessageBox.information(self, "Update Password Successful", "Password updated successfully.")
     
-                    self.password_lineEdit.clear() # clear password field
+                    # self.password_lineEdit.clear() # clear password field
                     self.hide_update_pass_form() # hide form
                 else:
                     QMessageBox.warning(self, "No Changes", "No changes were made to the user information.")
@@ -94,7 +94,7 @@ class ProfilePage(QWidget, profile_page):
     def cancel_password_clicked(self):
         if self.isPasswordEmpty():
             print('password field is empty')
-            self.password_lineEdit.clear() # clear password field
+            # self.password_lineEdit.clear() # clear password field
             self.hide_update_pass_form() # hide update password form
         else:
             print('password field is not empty')
@@ -105,21 +105,22 @@ class ProfilePage(QWidget, profile_page):
                 QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No, QMessageBox.StandardButton.Yes
             )
             if reply == QMessageBox.StandardButton.Yes:
-                self.password_lineEdit.clear() # clear password field
+                # self.password_lineEdit.clear() # clear password field
                 self.hide_update_pass_form() # hide update password form
             else:
                 print('update password process not cancelled')
 
     def hide_update_pass_form(self):
-        self.update_pass_form.hide()
+        # self.update_pass_form.hide()
+        pass
 
     def isFormClear(self):
         lineEdits = [
-            self.fname_lineEdit,
-            self.lname_lineEdit,
-            self.email_lineEdit,
-            self.address_lineEdit,
-            self.username_lineEdit
+            self.firstname_line,
+            self.lastname_line,
+            self.email_line,
+            self.address_line,
+            self.username_line
         ]
         for lineEdit in lineEdits:
             if lineEdit.text():
@@ -128,11 +129,11 @@ class ProfilePage(QWidget, profile_page):
     
     def clearForm(self):
         lineEdits = [
-            self.fname_lineEdit,
-            self.lname_lineEdit,
-            self.email_lineEdit,
-            self.address_lineEdit,
-            self.username_lineEdit
+            self.firstname_line,
+            self.lastname_line,
+            self.email_line,
+            self.address_line,
+            self.username_line
         ]
         for lineEdit in lineEdits:
             lineEdit.clear()
@@ -148,7 +149,7 @@ class ProfilePage(QWidget, profile_page):
             
             # hide form if the user clicked NO
             if reply == QMessageBox.StandardButton.No:
-                self.updateProfile_form.hide()
+                # self.updateProfile_form.hide()
                 self.clearForm()
             else:
                 # save new information
@@ -156,11 +157,12 @@ class ProfilePage(QWidget, profile_page):
                 self.clearForm()
         else:
             # if none hide form
-            self.updateProfile_form.hide()
+            # self.updateProfile_form.hide()
+            pass
 
     def show_update_profile_form(self):
         self.fill_update_form()
-        self.updateProfile_form.show()
+        # self.updateProfile_form.show()
 
     def display_user_info(self):
         try:
@@ -170,14 +172,14 @@ class ProfilePage(QWidget, profile_page):
 
         if document:
             # Update UI labels with document data
-            self.fullname_label.setText(f"{document.get('last_name')}, {document.get('first_name')}")
+            # self.fullname_label.setText(f"{document.get('last_name')}, {document.get('first_name')}")
             self.username_label.setText(document.get('username', ''))
             self.email_label.setText(document.get('email', ''))
-            self.accountRole_label.setText(document.get('user_type'))
-            self.accountStatus_label.setText(document.get('status'))
+            # self.accountRole_label.setText(document.get('user_type'))
+            # self.accountStatus_label.setText(document.get('status'))
             self.accountID_label.setText(document.get('account_id', ''))
             self.address_label.setText(document.get('address', ''))
-            self.lastLogin_label.setText(document.get('last_login', ''))
+            # self.lastLogin_label.setText(document.get('last_login', ''))
         else:
             QMessageBox.warning(self, "User Not Found", "The user information could not be found.")
 
@@ -193,23 +195,24 @@ class ProfilePage(QWidget, profile_page):
         except Exception as e:
             print(f"Error: {e}")
         if document:
-            self.username_lineEdit.setText(document.get('username'))
-            self.fname_lineEdit.setText(document.get('first_name'))
-            self.lname_lineEdit.setText(document.get('last_name'))
-            self.email_lineEdit.setText(document.get('email'))
-            self.address_lineEdit.setText(document.get('address'))
+            pass
+            # self.username_lineEdit.setText(document.get('username'))
+            # self.fname_lineEdit.setText(document.get('first_name'))
+            # self.lname_lineEdit.setText(document.get('last_name'))
+            # self.email_lineEdit.setText(document.get('email'))
+            # self.address_lineEdit.setText(document.get('address'))
         else:
             QMessageBox.warning(self, "User Not Found", "The user information could not be found.")
     
     def save_user_info(self):
         """Save the updated user information when the Save button is clicked."""
-        updated_data = {
-            'username': self.username_lineEdit.text().strip(),
-            'email': self.email_lineEdit.text().strip(),
-            'first_name': self.fname_lineEdit.text().strip(),
-            'last_name': self.lname_lineEdit.text().strip(),
-            'address': self.address_lineEdit.text().strip()
-        }
+        # updated_data = {
+        #     'username': self.username_lineEdit.text().strip(),
+        #     'email': self.email_lineEdit.text().strip(),
+        #     'first_name': self.fname_lineEdit.text().strip(),
+        #     'last_name': self.lname_lineEdit.text().strip(),
+        #     'address': self.address_lineEdit.text().strip()
+        # }
 
         # Remove any fields that are empty (optional)
         updated_data = {k: v for k, v in updated_data.items() if v}
@@ -230,7 +233,7 @@ class ProfilePage(QWidget, profile_page):
 
             # Update the current user's document in the collection using the stored username
             result = self.connect_to_db('accounts').update_one({'username': self.username}, {'$set': changes})
-            self.username = self.username_lineEdit.text().strip() # update value of self.username
+            # self.username = self.username_lineEdit.text().strip() # update value of self.username
 
             if result.modified_count > 0:
                 QMessageBox.information(self, "Update Successful", "User information updated successfully.")
